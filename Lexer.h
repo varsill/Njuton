22ce9bb2a137d9451ad4b78d5f42be69b2cff9d8
varsill <l00kasz99@onet.eu>
2017-04-22 12:30:47 +0200

    Update Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "Parser.h"
 using namespace std;
 class Lexer
 {
 public:
+	vector <Tree*> trees;
 	string str;
 	void lexer(string, string &);
 	int recognize(char);
 	Lexer(string);
 	string Lex();
-	vector <string> typy; //types of variables
+	vector <string> keywords;
+	void Create_Tree();
 };
 
 
